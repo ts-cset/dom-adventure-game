a7782b84f1d337fff6143cb660baff0981f7333d
Hari Allen <hariallen20@gmail.com>
2020-11-02 14:53:32 -0500

    Made actual progress this time

--- assets/main.js
+++ assets/main.js
@@ -4,4 +4,30 @@
 
 document.querySelector('#game');
 
-let path = document.querySelector("button");
+const room1 = document.querySelector('.room1');
+
+const leftP = document.querySelector('.leftP');
+const rightP = document.querySelector('.rightP');
+
+const leftB = document.querySelector('.leftB');
+const rightB = document.querySelector('.rightB');
+
+room1.textContent = 'This is the first room. Would you like to go left or right?';
+
+function left() {
+  room1.remove();
+  leftB.remove();
+  rightB.remove();
+
+  leftP.textContent = "HI";
+
+}
+
+function right() {
+  room1.remove();
+  leftB.remove();
+  rightB.remove();
+
+  rightP.textContent = "BYE";
+
+}
